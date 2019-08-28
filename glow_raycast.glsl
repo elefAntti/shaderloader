4b582b3f293ddd51664c75d371ee2a9c884a1952
Antti Valli <antti.valli@gmail.com>
2019-08-28 08:54:18 +0300

    Glow raycast algorithm

---
+++ glow_raycast.glsl
@@ -0,0 +1,49 @@
+
+float castRay( vec3 vStart, vec3 vDir, float my_time )
+{
+    float fCastLen = 0.0;
+    float fMinDist = 100.0;
+    vec3 vHit = vStart + vDir * 0.03;
+    while( fCastLen < 100.0 )
+    {
+        float fDistance = dist_model( vHit, my_time );
+        fMinDist = min(fMinDist, fDistance);
+        fCastLen += fDistance;
+        vHit = vDir * fCastLen + vStart;
+
+        if( fDistance < 0.001 )
+        {
+            return fMinDist;
+        }
+    }
+
+    return fMinDist;
+}
+
+vec3 glow( vec3 dist )
+{
+    float r = 0.25;
+    return smoothstep(0.0, 0.04, dist) * pow(abs(dist - 0.1) / r, vec3(-2.0));
+}
+
+vec4 rayTraceMain( vec2 fragCoord )
+{
+    float dt = 0.1;
+    vec2 position = fragCoord * -2.0 + 1.0;
+    vec3 cameraPos = vec3( 0, 1.0, -20.0 );
+    vec3 rayDir = normalize( vec3( position, 2.0 ) );
+    float dist_r = castRay( cameraPos, rayDir, time );
+    float dist_g = castRay( cameraPos, rayDir, time + dt);
+    float dist_b = castRay( cameraPos, rayDir, time + dt * 2.0);
+
+    return vec4(glow(vec3(dist_r, dist_g, dist_b)), 1.0);
+}
+
+void main()
+{
+    //Fix distortion due to screen ratio
+    aspect_ratio = image_width/image_height;
+    vec2 fixedCoord = (qt_TexCoord0 - vec2(0.5, 0.5)) * vec2(aspect_ratio, 1.0) + vec2(0.5, 0.5);
+
+    gl_FragColor = rayTraceMain( fixedCoord );
+}
