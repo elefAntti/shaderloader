fa800bde668c5d0933165e33c3638094108885f1
Antti Valli <antti.valli@gmail.com>
2019-08-31 10:48:02 +0300

    Create noise functions in utils

--- utils.glsl
+++ utils.glsl
@@ -9,10 +9,30 @@ vec3 rotate_y( vec3 pos, float angle )
 
 float triangle_wave(float x)
 {
-	return abs(mod(x, 4.0) - 2.0) - 1.0;
+    return abs(mod(x, 4.0) - 2.0) - 1.0;
 }
 
 float square_wave(float x)
 {
-	return sign(mod(x, 4.0) - 2.0);
+    return sign(mod(x, 4.0) - 2.0);
+}
+
+float noise(float pos)
+{
+    //Picking the low bits, which are more random
+    return mod(sin(pos) * 10000.0, 1.0);
+}
+
+float noise(vec2 pos)
+{
+    //Combine x and y so that there is no visible pattern in the noise
+    //Just picking two numbers that are not multiples of each other
+    float key = pos.x * 111.0 + pos.y * 788.0;
+    return noise(key);
+}
+
+float noise(vec3 pos)
+{
+    float key = pos.x * 111.0 + pos.y * 788.0 + pos.z * 827.0;
+    return noise(key);
 }
